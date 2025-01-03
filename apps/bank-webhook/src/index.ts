1c9b058c8b3c2d28151d66cbe919e670ba499498
Harshit Rai <raiharshit66@gmail.com>
2025-01-04 01:01:27 +0530

    Add the balance and Transaction models to the db

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -3,4 +3,6 @@ import express from 'express'
 const app = express();
 const port  = process.env.PORT || 3002
 app.use(express.json());
-app.listen(port)
+app.listen(port, ()=>{
+    console.log("Listening at " + port)
+})
