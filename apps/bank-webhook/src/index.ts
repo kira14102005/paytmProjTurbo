6bbf6009302174d76b9e8a08ab9f19d572d8c41a
Harshit Rai <raiharshit66@gmail.com>
2025-01-04 01:36:42 +0530

    Add the balance to model and update the status of onramptransac using prrisma Transactions :

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,8 +1,52 @@
+import prisma from '@repo/myDB/clients';
+import express from 'express';
 
-import express from 'express'
 const app = express();
-const port  = process.env.PORT || 3002
+const port = process.env.PORT || 3002;
+
 app.use(express.json());
-app.listen(port, ()=>{
-    console.log("Listening at " + port)
-})
+
+app.listen(port, () => {
+    console.log("Listening at " + port);
+});
+
+app.post('/webhook', async (req, res) => {
+    const paymentInfo = {
+        token: req.body.token,
+        userId: req.body.user,
+        amount: req.body.amount,
+    };
+
+    try {
+        // Validate inputs
+        if (!paymentInfo.token || !paymentInfo.userId || !paymentInfo.amount) {
+            return res.status(400).json({ error: 'Invalid input data' });
+        }
+
+        await prisma.$transaction([
+            prisma.balance.update({
+                where: {
+                    userId: paymentInfo.userId,
+                },
+                data: {
+                    amount: {
+                        increment: Number(paymentInfo.amount),
+                    },
+                },
+            }),
+            prisma.onRampTransac.update({
+                where: {
+                    token: paymentInfo.token,
+                },
+                data: {
+                    status: 'Success',
+                },
+            }),
+        ]);
+
+        res.status(200).json({ message: 'Transaction successful' });
+    } catch (e) {
+        console.error('Transaction error:', e);
+        res.status(411).json({ error: 'An error occurred during the transaction' });
+    }
+});
---
+++ packages/myDB/prisma/migrations/20250103195652_update_onramp/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[token]` on the table `OnRampTransac` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "OnRampTransac_token_key" ON "OnRampTransac"("token");
