3cc9ea1b284dd3f82b5c3516a1e1bdf2fd0c1a7c
Harshit Rai <raiharshit66@gmail.com>
2025-01-05 22:46:50 +0530

    Create the /transfer components AddMoneyCard ,OnrampTransac record

---
+++ apps/paytm-user/app/components/AddMoneyCard.tsx
@@ -0,0 +1,46 @@
+"use client"
+import { Button } from "@repo/ui/button";
+import { Card } from "@repo/ui/card";
+import { Center } from "@repo/ui/center";
+import { Select } from "@repo/ui/select";
+import { useState } from "react";
+import { TextInput } from "@repo/ui/textinput";
+
+
+const SUPPORTED_BANKS = [{
+    name: "HDFC Bank",
+    redirectUrl: "https://netbanking.hdfcbank.com"
+}, {
+    name: "Axis Bank",
+    redirectUrl: "https://www.axisbank.com/"
+}];
+
+export const AddMoney = () => {
+    const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [amount, setAmount] = useState(0)    //INTRODUCE the state variables amount and provider
+    const [provider , setprovider] = useState(SUPPORTED_BANKS[0]?.name || "")
+    return <Card title="Add Money">
+    <div className="w-full">
+        <TextInput label={"Amount"} placeholder={"Amount"} onChange={(e) => {   //SET the amount whenever input changes
+    setAmount(Number(e))
+        }} />
+        <div className="py-4 text-left">
+            Bank
+        </div>
+        <Select onSelect={(value) => {
+            setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+            setprovider(SUPPORTED_BANKS.find(x => x.name === value)?.name || "")
+        }} options={SUPPORTED_BANKS.map(x => ({
+            key: x.name,
+            value: x.name
+        }))} />
+        <div className="flex justify-center pt-4">
+            <Button onClick={async () => {
+              
+            }}>
+            Add Money
+            </Button>
+        </div>
+    </div>
+</Card>
+}
