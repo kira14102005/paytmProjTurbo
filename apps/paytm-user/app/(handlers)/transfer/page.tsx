67367964c02df08e5cdb964500474469d90cfc0c
Harshit Rai <raiharshit66@gmail.com>
2025-01-05 22:47:44 +0530

    Complete the transfer route page.tsx

---
+++ apps/paytm-user/app/(handlers)/transfer/page.tsx
@@ -0,0 +1,58 @@
+
+import { getServerSession } from "next-auth";
+import {  NEXTAUTH } from "../../lib/auth";
+import prisma from "@repo/myDB/clients";
+import { OnRampTransactions } from "../../components/OnRampTransactions";
+import { BalanceCard } from "../../components/BalanceCard";
+import { AddMoney } from "../../components/AddMoneyCard";
+
+async function getBalance() {
+    const session = await getServerSession(NEXTAUTH);
+    const balance = await prisma.balance.findFirst({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return {
+        amount: balance?.amount || 0,
+        locked: balance?.locked || 0
+    }
+}
+
+async function getOnRampTransactions() {
+
+    const session = await getServerSession(NEXTAUTH);
+    const txns = await prisma.onRampTransac.findMany({
+        where: {
+            userId: Number(session?.user?.id)
+        }
+    });
+    return txns.map(t => ({
+        time: t.startTime,
+        amount: t.amount,
+        status: t.status,
+        provider: t.provider
+    }))
+}
+
+export default async function() {
+    const balance = await getBalance();
+    const transactions = await getOnRampTransactions();
+
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+            Transfer
+        </div>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+            <div>
+                <AddMoney />
+            </div>
+            <div>
+                <BalanceCard amount={balance.amount} locked={balance.locked} />
+                <div className="pt-4">
+                    <OnRampTransactions transactions={transactions} />
+                </div>
+            </div>
+        </div>
+    </div>
+}
