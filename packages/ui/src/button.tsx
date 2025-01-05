6216552437b43c4f59b67e6936d61d90ddbac9bd
Harshit Rai <raiharshit66@gmail.com>
2025-01-05 22:39:52 +0530

    Add the More Ui components like CustomButton Select

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,17 +4,26 @@ import { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode;
-  className?: string;
-  appName: string;
+  onClick: () => void;
+}
+interface CustButtonProps {
+  children: ReactNode;
+  onClick: () => Promise<void>;
 }
 
-export const Button = ({ children, className, appName }: ButtonProps) => {
+export const Button = ({ onClick, children }: ButtonProps) => {
   return (
-    <button
-      className={className}
-      onClick={() => alert(`Hello from your ${appName} app!`)}
-    >
+    <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-4">
       {children}
     </button>
+
   );
 };
+export const CustButton = ({onClick , children} : CustButtonProps)=>{
+  return (
+    <button onClick={onClick} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-4">
+      {children}
+    </button>
+
+  );
+}
